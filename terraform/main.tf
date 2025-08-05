1579c7b2be2e6abe733a93127be49622aa812e19
harpreet1423 <harpreetkour1921@gmail.com>
2025-08-06 00:56:26 +0530

    updated  main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,10 +11,7 @@ resource "aws_ecr_repository" "job_portal" {
   }
 
   lifecycle {
-    ignore_changes = [
-    image_scanning_configuration,
-    image_tag_mutability
-  ]
+    ignore_changes = all
   }
 }
 
@@ -26,18 +23,72 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
       }
-    }]
+    ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_attach" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
 }
+
+
+
+# provider "aws" {
+#   region = var.aws_region
+# }
+
+# resource "aws_ecr_repository" "job_portal" {
+#   name                 = "job-portal"
+#   image_tag_mutability = "MUTABLE"
+
+#   image_scanning_configuration {
+#     scan_on_push = true
+#   }
+
+#   lifecycle {
+#     ignore_changes = [
+#     image_scanning_configuration,
+#     image_tag_mutability
+#   ]
+#   }
+# }
+
+# resource "aws_ecs_cluster" "job_portal_cluster" {
+#   name = "job-portal-cluster"
+# }
+
+# resource "aws_iam_role" "ecs_task_execution_role" {
+#   name = "ecsTaskExecutionRole"
+
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action    = "sts:AssumeRole"
+#       Effect    = "Allow"
+#       Principal = {
+#         Service = "ecs-tasks.amazonaws.com"
+#       }
+#     }]
+#   })
+# }
+
+# resource "aws_iam_role_policy_attachment" "ecs_task_execution_attach" {
+#   role       = aws_iam_role.ecs_task_execution_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# }
