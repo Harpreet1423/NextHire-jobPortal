6045a303ca40550a1eeaf106ac8e73ee2fcdfd63
harpreet1423 <harpreetkour1921@gmail.com>
2025-08-06 01:09:08 +0530

    updated  main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,12 +40,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.7.0"
     }
   }
-
-  required_version = ">= 1.3.0"
 }
+
 
 
 
